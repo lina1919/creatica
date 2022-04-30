bfce3a02b39695387b44aace5a59431dbc7f8c77
lina1919 <lina1919@naver.com>
2022-05-01 01:59:18 +0900

    changes

--- study/static/app.js
+++ study/static/app.js
@@ -13,42 +13,42 @@ var ls = document.getElementById('l_secs');
 var startTimer;
 var cycles = 0;
 
-start.addEventListener('click', function(){
-    if(startTimer === undefined){
+start.addEventListener('click', function () {
+    if (startTimer === undefined) {
         startTimer = setInterval(timer, 1000)
     } else {
         alert("Timer is already running");
     }
 })
 
-stop.addEventListener('click', function(){
+stop.addEventListener('click', function () {
     stopInterval()
     startTimer = undefined;
 })
 
 
 //Start Timer Function
-function timer(){
+function timer() {
     //Pomodoro Timer Countdown
-    if(ps.innerText != 0){
+    if (ps.innerText != 0) {
         ps.innerText -= 1;
-    } else if(pm.innerText != 0 && ps.innerText == 0){
+    } else if (pm.innerText != 0 && ps.innerText == 0) {
         ps.innerText = 59;
         pm.innerText -= 1;
     }
 
     //Short Break Timer Countdown
-    if(pm.innerText == 0 && ps.innerText == 0){
-        if(ss.innerText != 0){
+    if (pm.innerText == 0 && ps.innerText == 0) {
+        if (ss.innerText != 0) {
             ss.innerText -= 1;
-        } else if(sm.innerText != 0 && ss.innerText == 0){
+        } else if (sm.innerText != 0 && ss.innerText == 0) {
             ss.innerText = 59;
             sm.innerText -= 1;
             cycles += 1;
         }
     }
 
-    if (cycles > 2 && ps.innerText == 0 && pm.innerText == 0 && ss.innerText == 0 && sm.innerText == 0 && lm.innerText != 0 ) {
+    if (cycles > 2 && ps.innerText == 0 && pm.innerText == 0 && ss.innerText == 0 && sm.innerText == 0 && lm.innerText != 0) {
         if (ls.innerText != 0) {
             ls.innerText -= 1;
         }
@@ -56,7 +56,7 @@ function timer(){
             ls.innerText = 59;
             lm.innerText -= 1;
         }
-        
+
         cycles = 0;
         pm.innerText = 01;
         ps.innerText = "00";
@@ -64,7 +64,7 @@ function timer(){
         ss.innerText = "00";
         lm.innerText = 01;
         ls.innerText = "00";
-        
+
     }
     if (ps.innerText == 0 && pm.innerText == 0 && ss.innerText == 0 && sm.innerText == 0 && lm.innerText != 0 && cycles != 2) {
         pm.innerText = 01;
@@ -76,6 +76,6 @@ function timer(){
 }
 
 //Stop Timer Function
-function stopInterval(){
+function stopInterval() {
     clearInterval(startTimer);
 }
