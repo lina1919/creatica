afb6e35d0c686c3c0afda8e030c912c16ddc9cd4
lina1919 <lina1919@naver.com>
2022-05-01 10:47:29 +0900

    todolist

--- study/urls.py
+++ study/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('new/',views.new, name="new"),
+    path('delete',views.delete,name="delete"),
 ]
